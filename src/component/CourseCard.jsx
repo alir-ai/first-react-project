aaa8825f2432b97c0f65efbed873eb50aedc066b
Alir.Ai <alirezaahmdi11@gmail.com>
2023-12-16 11:56:21 +0330

    course-card components

--- src/component/CourseCard.jsx
+++ src/component/CourseCard.jsx
@@ -1,39 +1,51 @@
-function CourseCard(props) {
+function CourseCard({ course }) {
   return (
     <div className="course-item">
       <div className="course-item__img">
-        <img src={props.course.imageUrl} alt="image-1"></img>
+        <img src={course.imageUrl} alt={course.title}></img>
       </div>
       <div className="course-item__detail">
-        <div className="course-item__body">
-          <div>
-            <p className="title">{props.course.title}</p>
-            <p className="desc">{props.course.description}</p>
-          </div>
-          <span className="rate">{props.course.rate}</span>
-        </div>
-        <div className="course-item__footer">
-          <div className="tags">
-            {props.course.tags.map((t) => (
-              <span key={t} className="badge badge--secondary">
-                {props.course.tags}
-              </span>
-            ))}
-          </div>
-          <div className="caption">
-            <div className="date">
-              {new Date(props.course.start).toLocaleDateString("en-US", {
-                month: "short",
-                year: "numeric",
-                day: "numeric",
-              })}
-            </div>
-            <span className="badge badge--primary">{props.course.status}</span>
-          </div>
-        </div>
+        <CourseCardBody title={course.title} desc={course.description} rate={course.rate}/>
+        <CourseCardFooter course={course}/>
       </div>
     </div>
   );
 }
 
 export default CourseCard;
+
+function CourseCardBody({title, desc, rate}) {
+  return (
+    <div className="course-item__body">
+      <div>
+        <p className="title">{title}</p>
+        <p className="desc">{desc}</p>
+      </div>
+      <span className="rate">{rate}</span>
+    </div>
+  );
+}
+
+function CourseCardFooter({course}) {
+  return (
+    <div className="course-item__footer">
+      <div className="tags">
+        {course.tags.map((t) => (
+          <span key={t} className="badge badge--secondary">
+            {course.tags}
+          </span>
+        ))}
+      </div>
+      <div className="caption">
+        <div className="date">
+          {new Date(course.start).toLocaleDateString("en-US", {
+            month: "short",
+            year: "numeric",
+            day: "numeric",
+          })}
+        </div>
+        <span className="badge badge--primary">{course.status}</span>
+      </div>
+    </div>
+  );
+}
